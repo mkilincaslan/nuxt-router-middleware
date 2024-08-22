d85aff34e841ffd679f99166db91a8b61631a35c
Mesut KILINCASLAN <mesut@artelax.com>
2024-08-22 22:57:47 +0300

    define global middleware and remove local auth middleware

--- middleware/authenticate.global.ts
+++ middleware/authenticate.global.ts
@@ -1,7 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const auth = useCookie('user');
     
-    if (!auth.value) {
+    if (!auth.value && to.path !== '/login') {
         return navigateTo('/login');
+    }
+
+    if (auth.value && to.path == '/login') {
+        return navigateTo('/');
     }
 });
--- pages/admin.vue
+++ pages/admin.vue
@@ -7,6 +7,6 @@
 
 <script setup>
   definePageMeta({
-    middleware: ['authenticate', 'admin']
+    middleware: ['admin']
   });
 </script>
